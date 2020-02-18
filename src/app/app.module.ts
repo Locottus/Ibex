f2f115a6f2f1146853ac85e9be0a46e45fb1bea6
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-02-18 09:16:51 -0600

    agregado el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConstruccionComponent } from './construccion/construccion.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConstruccionComponent
+    ConstruccionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
